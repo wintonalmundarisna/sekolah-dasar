cba4be8589385bc76c24764b2d4ccbec7990efb1
ryanfkhrji <ryanfakhroji09@gmail.com>
2024-11-20 21:26:49 +0700

    bismillah

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
+
