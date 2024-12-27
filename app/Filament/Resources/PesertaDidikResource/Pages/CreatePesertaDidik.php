10a409a7766be5032ce51dd41e83406177ef42c8
Winton Almundarisna <wintonalmundarisna@gmail.com>
2024-12-27 12:54:40 +0700

    export pdf and zip

--- app/Filament/Resources/PesertaDidikResource/Pages/CreatePesertaDidik.php
+++ app/Filament/Resources/PesertaDidikResource/Pages/CreatePesertaDidik.php
@@ -11,8 +11,22 @@ use Filament\Tables\Actions\CreateAction;
 class CreatePesertaDidik extends CreateRecord
 {
     protected static string $resource = PesertaDidikResource::class;
-    
+
     protected static bool $canCreateAnother = false;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // dd($data);
+        $data['nik'] = "@ " . $data['nik'];
+        $data['telepon'] = "@ " . $data['telepon'];
+        $data['hp'] = "@ " . $data['hp'];
+        $data['nik-ayah'] = "@ " . $data['nik-ayah'];
+        $data['nik-ibu'] = "@ " . $data['nik-ibu'];
+        $data['nik-wali'] = "@ " . $data['nik-wali'];
+        $data['no-kk'] = "@ " . $data['no-kk'];
+
+        return $data;
+    }
 
     public function table(Table $table): Table
     {
